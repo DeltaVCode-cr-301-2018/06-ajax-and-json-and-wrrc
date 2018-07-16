7e7b01d0e1b049ca242df355f253e791140f29aa
tektechnologies <info@tektechnologies.com>
2018-07-16 15:30:33 -0500

    this adds json loaded

--- craig-ben/scripts/article.js
+++ craig-ben/scripts/article.js
@@ -12,8 +12,8 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+//// COMMENT: Why isn't this method written as an arrow function?
+// //Arrow functions treat this differently, Arrow functions do not work with (this)Arrow functions this is different from normal function expressions.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
@@ -21,7 +21,7 @@ Article.prototype.toHtml = function() {
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  ////The conditional (ternary) operator is the only JavaScript operator that takes three operands. This operator is frequently used as a shortcut for the if statement.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -32,8 +32,10 @@ Article.prototype.toHtml = function() {
 
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// COMMENT:
+//Where is this function called? in the article fetch function.
+//What does 'rawData' articleData represent now? data from the json file
+//How is this different from previous labs? The data is in a json file, not local file,
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -55,7 +57,13 @@ Article.fetchAll = () => {
 
   } else {
     // TODO: When we don't already have the rawData, we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?), cache it in localStorage so we can skip the server call next time, then load all the data into Article.all with the .loadAll function above, and then render the index page.
+    $.getJSON('./data/hackerIpsum.json').then(function (data) {
+      //console.log(data);
 
+      // Do something with the data we received!
+      Article.loadAll(data);
+      articleView.initIndexPage();
+    })
     // COMMENT: Discuss the sequence of execution in this 'else' conditional. Why are these functions executed in this order?
     // PUT YOUR RESPONSE HERE
   }
